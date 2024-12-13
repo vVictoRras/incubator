7154ce6132a6477e7bd0b1312fa50f982ea65759
Victor <you@example.com>
2024-12-13 22:28:32 +0200

    in progress

---
+++ 7/Band name generator.js
@@ -0,0 +1,16 @@
+
+function bandNameGenerator(str) {
+    const letter =['e','y','i','o','a'];
+    let name = str.split('');
+    for (let i=0;i<name.length;i++){
+     if (name[0]===letter[i]){
+         return str[0].toUpperCase()+ str.slice(1)+str.slice(1);
+     }
+
+    }
+     return ('The'+' '+ str[0].toUpperCase()+ str.slice(1))
+
+
+}
+
+console.log(bandNameGenerator("The Tart"));
